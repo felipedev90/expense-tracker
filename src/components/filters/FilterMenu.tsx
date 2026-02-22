1dcf2e9fa267ca7d95f5e4a2f0f5e42e2c14b692
Felipe Augusto <augusto.felipedev@gmail.com>
2026-02-22 17:45:21 -0300

    feat: create generic FilterMenu component with renderOption pattern

---
+++ src/components/filters/FilterMenu.tsx
@@ -0,0 +1,33 @@
+import { Check } from "lucide-react";
+import styles from "./Filters.module.css";
+
+interface FilterMenuProps<T> {
+  options: readonly T[];
+  selctedValue: T;
+  onSelect: (value: T) => void;
+  renderOption: (option: T) => React.ReactNode;
+}
+
+export function FilterMenu<T extends string>({
+  options,
+  selctedValue,
+  onSelect,
+  renderOption,
+}: FilterMenuProps<T>) {
+  return (
+    <div className={styles.menu}>
+      {options.map((option) => (
+        <button
+          key={option}
+          type="button"
+          className={styles.menuItem}
+          onClick={() => onSelect(option)}
+        >
+          <span className={styles.menuItemText}>{renderOption(option)}</span>
+
+          {selctedValue === option && <Check size={16} />}
+        </button>
+      ))}
+    </div>
+  );
+}
